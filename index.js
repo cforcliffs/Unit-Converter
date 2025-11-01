611e5655f6d0c01d11d6c6b5b7d26970620417bc
cforcliffs <cforcliffs@gmail.com>
2025-11-01 15:14:29 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+
+let myConversion = []
+
+const length = document.getElementById("Length")
+const volume = document.getElementById("Volume")
+const mass = document.getElementById("Mass")
+
+let inputEl = document.getElementById("input-el")
+
+const convertBtn = document.getElemenntById("convert-btn")
+
+
+
+
+
+    // trigger the function//
+
+    convertBtn.addEventListener("click", function(){
+
+        inputEl = ""
+    
+    
+    })
+
+
+
+
+
+
+//change the paragraphs acordiningly
+
+function convert(){
+
+
+
+    
+    }
+
+
+
+
+
+
+
+
